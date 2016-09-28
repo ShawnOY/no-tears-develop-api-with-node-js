fa47a86f0faa5d78370f56dd5f2e3966fddaf33a
Shawn OY <shawnoy@outlook.com>
2016-09-28 21:18:25 +0800

    修改隨堂練習3題目和解答

--- 隨堂練習3/Answer.js
+++ 隨堂練習3/Answer.js
@@ -1,14 +1,18 @@
 'use strict'
 
-for (let i = 1; i <= 10; i++) {
-  for (let j = 1; j <= i; j++) {
+let num = 1
+const minStars = 1
+const maxStars = 10
+
+for (; num <= maxStars; num++) {
+  for (let j = 1; j <= num; j++) {
     process.stdout.write('*')
   }
   process.stdout.write('\n')
 }
 
-for (let i = 10; i >= 1; i--) {
-  for (let j = 1; j <= i; j++) {
+for (; num >= minStars; num--) {
+  for (let j = 1; j <= num; j++) {
     process.stdout.write('*')
   }
   process.stdout.write('\n')
